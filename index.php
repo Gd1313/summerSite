7e03915e3864c0840fb6e0f06d77e0e3bcd57a4f
Gd1313 <gdvlad13@gmail.com>
2018-08-21 13:04:16 +0300

    add icon

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
   <head>
     <meta charset="utf-8">
     <title>Summer site</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+    <meta name="description" content="Practice project site">
+    <meta name="viewport" content="width=device-width, initial-scale=1"
+    <link href="img/sun.ico" rel="shortcut icon" type="image/x-icon">
   </head>
   <body>
 
