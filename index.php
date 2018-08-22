92ea1194adcf58296d97819f12d78816d0d8dca0
Gd1313 <gdvlad13@gmail.com>
2018-08-22 16:33:59 +0300

    fix wrong comment

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
 </div>
 </footer>
 </div>
-/*jQuery*/
+<!--jQuery-->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script>
   $('#menuShow').click (function(){
