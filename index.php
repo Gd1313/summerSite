68e1d55a7d5e6a593242beb4d5a71f9b2fb1fa2b
Gd1313 <gdvlad13@gmail.com>
2018-08-22 16:38:35 +0300

    fix link to ico

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Summer site</title>
     <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
     <meta name="description" content="Practice project site">
-    <meta name="viewport" content="width=device-width, initial-scale=1"
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="img/sun.ico" rel="shortcut icon" type="image/x-icon">
     <link rel="stylesheet" href="css/all.min.css">
   </head>
