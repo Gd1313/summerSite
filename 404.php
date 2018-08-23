16f9f1bd6d9507ab07c3bcd1c2ac95d53529888c
Gd1313 <gdvlad13@gmail.com>
2018-08-23 16:40:49 +0300

    add error 404 page

---
+++ 404.php
@@ -0,0 +1,39 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Error 404</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+    <link rel="stylesheet" href="css/aside.css" type="text/css" charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="sun.ico" rel="shortcut icon" type="image/x-icon">
+    <link rel="stylesheet" href="css/all.min.css">
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="content">
+<?php require_once"blocks/header.php"  ?>
+<div id="main">
+    <div id="news">
+      <h2 class="heading">Error 404/h2>
+      <div style="clear:both"><br></div>
+      <div class="block">
+
+Oops .. Something went wrong and you got to mistake 404! Perhaps the page you were trying to navigate to was deleted or it never existed! You can use the search on the site or go to the main page- <a href="https://mysummersite.000webhostapp.com/"></a>
+      </div>
+    </div>
+</div>
+<?php require_once"blocks/aside.php"  ?>
+<div style="clear both">
+  <br>
+</div>
+
+
+</div>
+  <?php require_once"blocks/footer.php"  ?>
+</div>
+<?php require_once"blocks/JS_scripts.php"  ?>
+  </body>
+</html>
