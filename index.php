6d82f20bfebb1150eb17e7cd3ce8001fe97df0e7
Gd1313 <gdvlad13@gmail.com>
2018-08-23 13:28:36 +0300

    add aside styles and fix it

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Summer site</title>
     <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
     <link rel="stylesheet" href="css/main page.css" type="text/css" charset="utf-8">
+      <link rel="stylesheet" href="css/aside.css" type="text/css" charset="utf-8">
     <meta name="description" content="Practice project site">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="sun.ico" rel="shortcut icon" type="image/x-icon">
